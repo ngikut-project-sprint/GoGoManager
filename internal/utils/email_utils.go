39d9a90b99ba4dcab47cdd614e1e1bd82d6ca7a3
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-01-08 22:58:43 +0700

    feat: add email validator

---
+++ internal/utils/email_utils.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"errors"
+	"net"
+	"net/mail"
+	"regexp"
+	"strings"
+)
+
+func ValidateEmail(email string) error {
+	// Regex structure validation
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+	if !re.MatchString(email) {
+		return errors.New("Invalid email format")
+	}
+
+	// Parsing email using net/mail
+	_, err := mail.ParseAddress(email)
+	if err != nil {
+		return errors.New("Invalid email structure (RFC compliance)")
+	}
+
+	// Domain validation
+	parts := strings.Split(email, "@")
+	if len(parts) != 2 {
+		return errors.New("Invalid email format (missing domain)")
+	}
+
+	domain := parts[1]
+	maxRecords, err := net.LookupMX(domain)
+	if err != nil || len(maxRecords) == 0 {
+		return errors.New("Domain does not exist or no MX records found")
+	}
+
+	return nil
+}
