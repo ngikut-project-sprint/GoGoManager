0a2ae70b0a8cba4d803f5e2552b648768ca6f92e
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-01-08 23:03:12 +0700

    feat: add routers

--- internal/routes/route.go
+++ internal/routes/route.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/ngikut-project-sprint/GoGoManager/internal/handlers"
+	"github.com/ngikut-project-sprint/GoGoManager/internal/repositories"
+	"github.com/ngikut-project-sprint/GoGoManager/internal/services"
+)
+
+func NewRouter(db *sql.DB) *http.ServeMux {
+	mux := http.NewServeMux()
+	ManagerRouter(mux, db)
+	return mux
+}
+
+func ManagerRouter(mux *http.ServeMux, db *sql.DB) {
+	repo := repositories.NewManagerRepository(db)
+	service := services.NewManagerService(repo)
+	AuthRouter(mux, service)
+}
+
+func AuthRouter(mux *http.ServeMux, manager_service services.ManagerService) {
+	handler := handlers.NewAuthHandler(manager_service)
+	mux.HandleFunc("/auth", handler.Auth)
+}
