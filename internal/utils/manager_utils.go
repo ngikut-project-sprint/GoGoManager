4738e3af51a7bed9aec08dca1d59efa3eb4290c2
deni sugiarto <sugiarto.deni@gmail.com>
2025-01-10 07:22:03 +0700

    feat(manager_utils): create manager response

---
+++ internal/utils/manager_utils.go
@@ -0,0 +1,9 @@
+package utils
+
+type ManagerResponse struct {
+	Email           string `json:"email"`
+	Name            string `json:"name"`
+	UserImageUri    string `json:"userImageUri"`
+	CompanyName     string `json:"companyName"`
+	CompanyImageUri string `json:"companyImageUri"`
+}
