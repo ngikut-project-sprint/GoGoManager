8d5c7f16af3afe7c543b59e89f188b62a45e4e63
fardan <fardan.wardhana@gmail.com>
2025-01-13 23:01:52 +0700

    fixing: department test-scenario

--- internal/models/deparment.go
+++ internal/models/deparment.go
@@ -3,28 +3,28 @@ package models
 import "time"
 
 type Department struct {
-    ID        int       `json:"department_id"`
-    Name      string    `json:"name"`
-    ManagerID int       `json:"manager_id"`
-    CreatedAt time.Time `json:"created_at"`
-    UpdatedAt time.Time `json:"updated_at"`
+	ID        int       `json:"department_id"`
+	Name      string    `json:"name"`
+	ManagerID int       `json:"manager_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type CreateDepartmentRequest struct {
-    Name string `json:"name" validate:"required,min=4,max=33"`
+	Name string `json:"name" validate:"required,min=4,max=33"`
 }
 
 type UpdateDepartmentRequest struct {
-    Name string `json:"name" validate:"required,min=4,max=33"`
+	Name string `json:"name" validate:"required,min=4,max=33"`
 }
 
 type DepartmentResponse struct {
-    DepartmentId string `json:"departmentId"`
-    Name         string `json:"name"`
+	DepartmentId string `json:"departmentId"`
+	Name         string `json:"name"`
 }
 
 type GetDepartmentQuery struct {
-    Limit  int    `query:"limit"`
-    Offset int    `query:"offset"`
-    Name   string `query:"name"`
+	Limit  int    `query:"limit"`
+	Offset int    `query:"offset"`
+	Name   string `query:"name"`
 }
